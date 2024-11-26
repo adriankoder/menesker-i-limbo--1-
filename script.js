358a04eaa07352105796138b01226591f99e967f
Ditt Navnadriankoder <adrian.koder@gmail.com>
2024-11-26 17:19:12 +0100

    rettskrining utkast 16

--- script.js
+++ script.js
@@ -9,23 +9,30 @@ const KontaktOss = document.getElementById("Kontakt");
 
 hovedside.addEventListener("click", () => {
   meneskerILombo.innerHTML =
-    "<p> Mennesker i limbo Bergen er en organisaskjon for lengeværende avsylsøkere med endelig avslag. Vi kjemper for å bedre vår situasjon. Gruppen vår består av både kvinner og menn. Noen av oss har barn, andre kom til norge som mindreårige. Vi kommer fra forskjellige land og har ulik bakrunn. Det vi har felles, er at vi lever i Norge uten rett til: </p>";
-  // "<p>Velkommen til hjemmesiden vår</p> <p>Her kan du lese om arbeidet vårt og hvordan du kan hjelpe</p> <p>Vi jobber for å hjelpe mennesker som har flyktet fra krig og forfølgelse</p>";
+    "<p>Mennesker i limbo Bergen er en organisasjon for lengeværende asylsøkere med endelig avslag. Vi kjemper for å bedre vår situasjon. Gruppen vår består av både kvinner og menn. Noen av oss har barn, andre kom til Norge som mindreårige. Vi kommer fra forskjellige land og har ulik bakgrunn. Det vi har felles, er at vi lever i Norge uten rett til:</p>";
 });
+
 omArbeidet.addEventListener("click", () => {
-  meneskerILombo.innerHTML = alert("test");
+  meneskerILombo.innerHTML = "Dette er en test";
 });
 
 VaneligeSpørsmål.addEventListener("click", () => {
   meneskerILombo.innerHTML =
-    "<button id = vaneligSpærsmålHvorfor> hvorfor reiser de ikke bare tilbake?</button>";
-  vaneligSpærsmålHvorfor.addEventListener("click", () => {
+    "<button id='vaneligSpørsmålHvorfor'>Hvorfor reiser de ikke bare tilbake?</button>";
+  const vaneligSpørsmålHvorfor = document.getElementById(
+    "vaneligSpørsmålHvorfor"
+  );
+  vaneligSpørsmålHvorfor.addEventListener("click", () => {
     meneskerILombo.innerHTML =
-      "<p>det er grove brudd på meneskerettigheter der jeg kommer fra</p>som kvinne mangler jeg rettigheter i hjemlandet mitt<p></p> <p>mitt land er styrt av diktatur og er i fare for å bli satt i fengsel og torturert</p>";
+      "<p>Det er grove brudd på menneskerettigheter der jeg kommer fra.</p>" +
+      "<p>Som kvinne mangler jeg rettigheter i hjemlandet mitt.</p>" +
+      "<p>Mitt land er styrt av diktatur, og jeg er i fare for å bli satt i fengsel og torturert.</p>";
   });
 });
 
 KontaktOss.addEventListener("click", () => {
   meneskerILombo.innerHTML =
-    "<p>kontakt oss på telefon 12345678 eller epost:</p> <a href='mailto<>@gmail.com'>exemple@gmail.com</a> <p> støtt oss på <a href='https://www.vipps.no/'>vipps</a> </p>";
+    "<p>Kontakt oss på telefon: <a href='tel:+4790038238233'>90038238233</a></p>" +
+    "<p>Eller e-post: <a href='mailto:example@gmail.com'>example@gmail.com</a></p>" +
+    "<p>Støtt oss på <a href='https://www.vipps.no/'>Vipps</a></p>";
 });
