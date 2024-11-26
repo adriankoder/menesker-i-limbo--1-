acabeb550c34f4ffd149b499ba3187ff62537d5e
Ditt Navnadriankoder <adrian.koder@gmail.com>
2024-11-26 18:04:22 +0100

    utkast 16  fjerner li

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ const VaneligeSpørsmål = document.getElementById("VaneligeSpørsmål");
 const vaneligSpørsmålHvorfor = document.getElementById(
   "vaneligSpørsmålHvorfor"
 );
+const liID = document.getElementById("liID");
 const KontaktOss = document.getElementById("Kontakt");
 
 hovedside.addEventListener("click", () => {
   meneskerILombo.innerHTML =
     "<p>Mennesker i limbo Bergen er en organisasjon for lengeværende asylsøkere med endelig avslag. Vi kjemper for å bedre vår situasjon. Gruppen vår består av både kvinner og menn. Noen av oss har barn, andre kom til Norge som mindreårige. Vi kommer fra forskjellige land og har ulik bakgrunn. Det vi har felles, er at vi lever i Norge uten rett til:</p>";
+  liID.style.display = "block";
 });
 
 omArbeidet.addEventListener("click", () => {
   meneskerILombo.innerHTML = "Dette er en test";
+  liID.style.display = "none";
 });
 
 VaneligeSpørsmål.addEventListener("click", () => {
@@ -22,11 +25,14 @@ VaneligeSpørsmål.addEventListener("click", () => {
   const vaneligSpørsmålHvorfor = document.getElementById(
     "vaneligSpørsmålHvorfor"
   );
+  liID.style.display = "none";
+
   vaneligSpørsmålHvorfor.addEventListener("click", () => {
     meneskerILombo.innerHTML =
       "<p>Det er grove brudd på menneskerettigheter der jeg kommer fra.</p>" +
       "<p>Som kvinne mangler jeg rettigheter i hjemlandet mitt.</p>" +
       "<p>Mitt land er styrt av diktatur, og jeg er i fare for å bli satt i fengsel og torturert.</p>";
+    liID.style.display = "none";
   });
 });
 
@@ -35,4 +41,5 @@ KontaktOss.addEventListener("click", () => {
     "<p>Kontakt oss på telefon: <a href='tel:+4790038238233'>90038238233</a></p>" +
     "<p>Eller e-post: <a href='mailto:example@gmail.com'>example@gmail.com</a></p>" +
     "<p>Støtt oss på <a href='https://www.vipps.no/'>Vipps</a></p>";
+  liID.style.display = "none";
 });
